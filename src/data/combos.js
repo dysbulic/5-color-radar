350cc49c6faa469a4578b03e3c9aee117ee5aa9d
Will Holcomb <dys@dhappy.org>
2021-02-26 00:26:04 -0500

    contemplatin' a spinner ⚒

--- src/data/combos.js
+++ src/data/combos.js
@@ -5,26 +5,26 @@ export default {
 	0b00100: 'Black',
 	0b00010: 'Red',
 	0b00001: 'Green',
-  0b11000: 'Azorius',    // WU
-  0b10100: 'Orzhov',     // WB
-  0b10010: 'Boros',      // WR
-  0b10001: 'Selesnya',   // WG
-  0b01100: 'Dimir',      // UB
-  0b01010: 'Izzet',      // UR
-  0b01001: 'Simic',      // UG
-  0b00110: 'Rakdos',     // BR
-  0b00101: 'Golgari',    // BG
-  0b00011: 'Gruul',      // RG
-  0b11100: 'Esper',      // WUB
-  0b11010: 'Jeskai',     // WUR
-  0b11001: 'Bant',       // WUG
-  0b10110: 'Mardu',      // WBR
-  0b10101: 'Abzan',      // WBG
-  0b10011: 'Naya',       // WRG
-  0b01101: 'Sultai',     // UBG
-  0b01110: 'Grixis',     // UBR
-  0b00111: 'Jund',       // BRG
-  0b01011: 'Temur',      // URG
+  0b11000: 'Azorius Senate',    // WU
+  0b10100: 'Orzhov Syndicate',  // WB
+  0b10010: 'Boros Legion',      // WR
+  0b10001: 'Selesnya Conclave', // WG
+  0b01100: 'House Dimir',       // UB
+  0b01010: 'Izzet League',      // UR
+  0b01001: 'Simic Combine',     // UG
+  0b00110: 'Cult of Rakdos',    // BR
+  0b00101: 'Golgari Swarm',     // BG
+  0b00011: 'Gruul Clans',       // RG
+  0b11100: 'Esper Shard',       // WUB
+  0b11010: 'Jeskai Way',        // WUR
+  0b11001: 'Bant Shard',        // WUG
+  0b10110: 'Mardu Horde',       // WBR
+  0b10101: 'Abzan Houses',      // WBG
+  0b10011: 'Naya Shard',        // WRG
+  0b01101: 'Sultai Brood',      // UBG
+  0b01110: 'Grixis Shard',      // UBR
+  0b00111: 'Jund Shard',        // BRG
+  0b01011: 'Temur Frontier',    // URG
   0b11110: 'Artifice',   // WUBR
   0b11101: 'Growth',     // WUBG
   0b11011: 'Altruism',   // WURG
