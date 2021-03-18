13be0c64e9d484c50e4e64336fc27f221e16bfc2
Will Holcomb <dys@dhappy.org>
2021-03-18 01:15:50 -0400

    creditin', licensin', & removin' copyrighted work 📜

--- src/About.jsx
+++ src/About.jsx
@@ -3,6 +3,7 @@ import { Flex, Heading, Link, Text } from '@chakra-ui/react'
 export default () => (
   <Flex direction="column" justify="center" maxW="min(100vw, 25rem)" m="auto">
     <Heading m={5} textAlign="center">Magic: The Gathering's Color Dispositions</Heading>
+    <Text>The questions and scoring are originally from <Link href="https://jerdle-code.github.io/color-pie-test/">@Jerdle-code's Color Pie Test</Link>.</Text>
     <Text>The information and non-censored artworks are made available though Wizards of the Coast's <Link href="https://company.wizards.com/en/legal/fancontentpolicy">Fan Content Policy</Link>.</Text>
     <Text>This site is neither approved of nor endorsed by Wizards. Portions of the materials used are © Wizards of the Coast LLC.</Text>
   </Flex>
